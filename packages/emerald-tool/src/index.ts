6a5b2fb599e00f85d02ae0e58238f0af0e665d1b
gagarin55 <gagarin55@protonmail.com>
2018-05-17 17:53:22 +0300

    TxView moved to emerald-tool

--- packages/emerald-tool/src/index.ts
+++ packages/emerald-tool/src/index.ts
@@ -1,5 +1,6 @@
 export {default as AddressView} from './explorer/components/AddressView';
 export {default as BlockList} from './explorer/components/BlockList';
+export {default as TxView} from './explorer/components/TxView';
 
 export {default as Navigation} from './navigation';
 
