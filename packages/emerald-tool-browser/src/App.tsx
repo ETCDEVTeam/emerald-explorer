12372a7a4155cbc5d2f7b36481a2cfc20c2eb782
Zachary Belford <belfordz66@gmail.com>
2018-08-14 17:24:21 -0700

    wip

--- packages/emerald-tool-browser/src/App.tsx
+++ packages/emerald-tool-browser/src/App.tsx
@@ -13,27 +13,41 @@ import Contract from './containers/Contract';
 import DeployContract from './containers/DeployContract';
 import { history } from './store';
 
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import { theme } from 'emerald-js-ui/lib/theme';
+import { MuiThemeProvider, withTheme } from '@material-ui/core/styles';
+import { AppBar } from 'emerald-js-ui';
+import theme from 'emerald-js-ui/lib/theme';
 
-class App extends React.Component {
-
+class App extends React.Component<theme> {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Header />
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route exact={true} path="/" component={Dashboard} />
-            <Route path="/node/:id/contracts/deploy" component={DeployContract} />
-            <Route path="/node/:id/contracts/:address" component={Contract} />
-            <Route path="/node/:id/contracts" component={ContractList} />
-            <Route path="/node/:id/block/:hash" component={Block} />
-            <Route path="/node/:id/tx/:hash" component={Transaction} />
-            <Route path="/node/:id/address/:hex" component={Address} />
-            <Route path="/node/:id" component={NodeView} />
-          </Switch>
-        </ConnectedRouter>
+        <div>
+          <div>
+            <AppBar
+              title="Emerald Tool"
+              subtitle="wut"
+              blockNumber="7331"
+              fiatBalance="200"
+              fiatSymbol="USD"
+              balance="20"
+              symbol="ETC"
+            />
+          </div>
+          <div style={{ margin: '20px' }}>
+            <ConnectedRouter history={history}>
+              <Switch>
+                <Route exact={true} path="/" component={Dashboard} />
+                <Route path="/node/:id/contracts/deploy" component={DeployContract} />
+                <Route path="/node/:id/contracts/:address" component={Contract} />
+                <Route path="/node/:id/contracts" component={ContractList} />
+                <Route path="/node/:id/block/:hash" component={Block} />
+                <Route path="/node/:id/tx/:hash" component={Transaction} />
+                <Route path="/node/:id/address/:hex" component={Address} />
+                <Route path="/node/:id" component={NodeView} />
+              </Switch>
+            </ConnectedRouter>
+          </div>
+        </div>
       </MuiThemeProvider>
     );
   }
